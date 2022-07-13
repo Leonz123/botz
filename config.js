4626ad220e442cdd615423d2ddb3f1051edd7390
TTMgamers <92926856+TTMgamers@users.noreply.github.com>
2022-07-14 02:26:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -33,15 +33,15 @@ global.nomer = 'https://wa.me/6283829386551' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 15.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 25.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
+global.fotonya1 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini juga ganti pakai foto bot mu
+global.fotonya3 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e879.jpg' //ini juga ganti
 global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
-global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379e.jpg' //ganti pakai poto payment atau apa
+global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '❋ཻུ۪۪Togabotz-Ofc⳹ ❋ཻུ۪۪'
